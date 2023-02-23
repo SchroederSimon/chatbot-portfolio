5b1a17cfeeb48c7c8d1c8c6da30fd0f06ed8acd1
SchroederSimon <schroeder.ssimon@gmail.com>
2023-02-23 20:01:56 -0300

    almost completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css'
 
 type Message = {
   id: string;
   type: "bot" | "user";
-  text: string;
+  text: React.ReactNode;
 }
 
+const ANSWERS = {
+  Introduction: (
+    <p>Soy simon</p>
+  ),
+  Experience: (
+    <p>Soy experience</p>
+  ),
+  Contact: (
+    <p>Soy Contact</p>
+  ),
+  Projects: (
+    <p>Soy Projects</p>
+  ),
+  Personal: (
+    <p>Soy Personal</p>
+  ),
+  Unknown: (
+    <p>Soy Unknown</p>
+  )
+}
 const EXAMPLES = [{ "text": "Hi", "label": "Introduction" }, { "text": "Who are you?", "label": "Introduction" }, { "text": "Want to work with you", "label": "Introduction" }, { "text": "Where are you from?", "label": "Introduction" }, { "text": "Are you looking a job?", "label": "Introduction" }, { "text": "Wich technology do you use?", "label": "Experience" }, { "text": "What languaje do you know?", "label": "Experience" }, { "text": "Where i can reach you?", "label": "Contact" }, { "text": "How is your LinkedIn profile?", "label": "Contact" }, { "text": "How is your GitHub profile?", "label": "Contact" }, { "text": "When you start programming?", "label": "Experience" }, { "text": "What inspired you to pursue a career in this field?", "label": "Experience" }, { "text": "Can you tell me what\'s your bigges project so far?", "label": "Projects" }, { "text": "What are your strenghts and weaknesses as a developer?", "label": "Personal" }, { "text": "dasdad", "label": "Unknown" }, { "text": "Do you have a resumee?", "label": "Contact" }, { "text": "Age of your cats", "label": "Unknown" }, { "text": "Weak and nesses", "label": "Personal" }, { "text": "Wich projects do you have?", "label": "Projects" }]
 const API_KEY = ""
 
@@ -17,20 +37,27 @@ function App() {
     {
       id: "1",
       type: "bot",
-      text: "Hola soy un bot"
-    },
-    {
-      id: "2",
-      type: "user",
-      text: "Hola soy un usuario"
+      text: "Hi, im X and i will answer some questions about Simon."
     }
   ]);
 
   const [question, setQuestion] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
+  const container = useRef<HTMLDivElement>(null);
 
   async function handleSubmit(event: React.FormEvent) {
+
     event.preventDefault();
-    fetch("https://api.cohere.ai/classify", {
+
+    if (loading) return;
+
+    setLoading(true);
+    setMessages((messages) =>
+      messages.concat({ id: String(Date.now()), type: "user", text: question })
+    );
+    setQuestion("");
+
+    const { classifications } = await fetch("https://api.cohere.ai/classify", {
       method: "POST",
       headers: {
         Authorization: `Bearer ${API_KEY}`,
@@ -41,13 +68,26 @@ function App() {
         inputs: ["question"],
         examples: EXAMPLES,
       })
-    });
+    }).then(res => res.json());
+
+    setMessages((messages) =>
+      messages.concat({
+        id: String(Date.now()),
+        type: "bot",
+        text: ANSWERS[classifications[0].prediction as keyof typeof ANSWERS] || ANSWERS["Unknown"]
+      })
+    );
+    setLoading(false);
   }
+
+  useEffect(() => {
+    container.current?.scrollTo(0, container.current.scrollHeight)
+  }, [messages]);
 
   return (
     <div className="chat-container">
       <h1>Chatbot</h1>
-      <div className="chat-bot">
+      <div ref={container} className="chat-bot">
         {
           messages.map((message) => (
             <div className={`chat-messages ${message.type === "bot" ? "bot-message" : "user-message"}`} key={message.id}>
@@ -57,14 +97,14 @@ function App() {
         }
       </div>
       <form onSubmit={handleSubmit}>
-        <input 
-        type="text" 
-        placeholder="Ask me something" 
-        name="question"
-        value={question}
-        onChange={(event) => setQuestion(event.target.value)}
+        <input
+          type="text"
+          placeholder="Ask me something"
+          name="question"
+          value={question}
+          onChange={(event) => setQuestion(event.target.value)}
         />
-        <button type="submit">Send</button>
+        <button disabled={loading} type="submit" className={`button-loading ${loading ? "button-loading" : "button"}`}>Send</button>
       </form>
     </div>
   )
